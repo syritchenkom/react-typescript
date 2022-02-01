0f4620feb76f656390d0ab401790ce0c1563383f
Mykhailo Syritchenko <syritchenko@gmail.com>
2022-02-01 23:23:31 +0100

    Modified src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,57 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Card, { CardVariant } from './components/Card';
-import UserList from './components/UserList';
-import { IUser } from './types/types';
+// import UserList from './components/UserList';
+import { ITodo, IUser } from './types/types';
+import axios from 'axios';
+import List from './components/List';
+import UserItem from './components/UserItem';
+import TodoItem from './components/TodoItem';
+import EventsExample from './components/EventsExample';
+import { BrowserRouter, Route, NavLink } from 'react-router-dom';
+import UserPage from './components/UserPage';
+import TodosPage from './components/TodosPage';
+import UserItemPage from './components/UserItemPage';
+import TodoItemPage from './components/TodoItemPage';
 
 const App = () => {
-	const users: IUser[] = [
-		{
-			id: 1,
-			name: 'Alex Shall',
-			email: 'ash@mail.com',
-			address: { city: 'Warszaw', street: 'Grono', zipcode: '1234' }
-		},
-		{
-			id: 2,
-			name: 'Adom Shill',
-			email: 'ash@mail.com',
-			address: { city: 'Wroclaw', street: 'Granda', zipcode: '124' }
-		}
-	];
+	// 00:38:00  React TypeScript ПОЛНЫЙ КУРС 2021. Props, Events, Router, Hooks, Requests.
+
+	// const users: IUser[] = [
+	// 	{
+	// 		id: 1,
+	// 		name: 'Alex Shall',
+	// 		email: 'ash@mail.com',
+	// 		address: { city: 'Warszaw', street: 'Grono', zipcode: '1234' }
+	// 	},
+	// 	{
+	// 		id: 2,
+	// 		name: 'Adom Shill',
+	// 		email: 'ash@mail.com',
+	// 		address: { city: 'Wroclaw', street: 'Granda', zipcode: '124' }
+	// 	}
+	// ];
+
 	return (
-		<div>
-			<Card
-				// onClick={(num) => console.log('click', num)}
-				variant={CardVariant.outlined}
-				width="200px"
-				height="200px">
-				<button>Button</button>
+		<BrowserRouter>
+			<div>
 				<div>
-					Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consectetur
-					odio numquam inventore quod voluptate reiciendis delectus pariatur
-					vitae repudiandae modi nobis mollitia perspiciatis doloremque
-					voluptas, facere corrupti nihil at? Tempore?
+					<NavLink to={'/users'}>Users</NavLink>
+					<NavLink to="/todos">Todo list</NavLink>
 				</div>
-			</Card>
-			<UserList users={users} />
-		</div>
+				<Route path={'/users'} exact>
+					<UserPage />
+				</Route>
+				<Route path={'/todos'} exact>
+					<TodosPage />
+				</Route>
+				<Route path={'/users/:id'}>
+					<UserItemPage />
+				</Route>
+				<Route path={'/todos/:id'}>
+					<TodoItemPage />
+				</Route>
+			</div>
+		</BrowserRouter>
 	);
 };
 
