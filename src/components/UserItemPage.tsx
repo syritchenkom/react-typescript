b2b98e15744f897b142c20c9f0562c68c5da5569
Mykhailo Syritchenko <syritchenko@gmail.com>
2022-02-02 23:46:16 +0100

    Modified and added new file src/components/UserItemPage.tsx

---
+++ src/components/UserItemPage.tsx
@@ -0,0 +1,41 @@
+import axios from 'axios';
+import React, { FC, useEffect, useState } from 'react';
+import { IUser } from '../types/types';
+import { useHistory, useParams } from 'react-router-dom';
+
+interface UserItemPageParams {
+	id: string;
+}
+
+const UserItemPage: FC = () => {
+	const [user, setUser] = useState<IUser | null>(null);
+	const params = useParams<UserItemPageParams>();
+	const history = useHistory();
+
+	useEffect(() => {
+		fetchUser();
+	}, []);
+
+	async function fetchUser() {
+		try {
+			const response = await axios.get<IUser>(
+				'https://jsonplaceholder.typicode.com/users/' + params.id
+			);
+			setUser(response.data);
+		} catch (e) {
+			alert(e);
+		}
+	}
+	return (
+		<div>
+			<button onClick={() => history.push('/users')}>Back</button>
+			<h1>User pages {user?.name}</h1>
+			<div>{user?.email}</div>
+			<div>
+				{user?.address.city} {user?.address.street} {user?.address.zipcode}
+			</div>
+		</div>
+	);
+};
+
+export default UserItemPage;
